8c83b4484c1d81b56ba5f55b9dd844fe827990f1
Kacper <43350503+KacperBlotny@users.noreply.github.com>
2018-09-17 20:00:34 +0200

    Add files via upload

---
+++ main.cpp.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Zadanie 3.3" <<endl;
+
+    cout <<endl;
+
+    cout <<"wynik pkt a to: " << 3*7/2-1<<endl;
+    cout <<"wynik pkt b to: " << 3*7.0/2-1 <<endl;
+    cout <<"wynik pkt c to: " << (3*7)/2-1 <<endl;
+    cout <<"wynik pkt d to: " << 3*(7/2.0-1) <<endl;
+    cout <<"wynik pkt e to: " << 3*(7/2)-1 <<endl;
+    cout <<"wynik pkt f to: " << 3*7.0/(2-1) <<endl;
+    cout <<"wynik pkt g to: " << 124%12-2*4 <<endl;
+    cout <<"wynik pkt h to: " << 4*105%10/2*108 <<endl;
+    cout <<"wynik pkt i to: " << (-8-13*2)/2.5 <<endl;
+    cout <<"wynik pkt j to: " << (pow(2.5,3)+17%5)-sqrt(4) <<endl;
+    cout <<"wynik pkt k to: " << 12%5+2%5 <<endl;
+    cout <<"wynik pkt l to: " << 5+4*3.0/5 <<endl;
+    cout <<"wynik pkt m to: " << 2%5*3-4 <<endl;
+    cout <<"wynik pkt n to: " << 7/3%2+6 <<endl;
+    cout <<"wynik pkt o to: " << 10-4+2/2*3 <<endl;
+    cout <<"wynik pkt p to: " << 11/3%5+1 <<endl;
+    cout <<"wynik pkt q to: " << 4-5/4.0*2 <<endl;
+
+    cout <<endl;
+
+    cout << "Koniec zad 3.3" <<endl;
+
+                                            cout <<endl;
+
+                                            cout <<endl;
+
+                                            cout <<endl;
+
+                                            cout <<endl;
+
+                                            cout <<endl;
+
+    int a, b, c;
+
+    cout << "Zadanie 3.4" <<endl;
+
+    cout <<endl;
+
+    cout << "wynik pkt a to: " <<  sqrt(7/((a^3)+cos(b)))  <<endl;
+
+    cout << "wynik pkt b to: " << sin(((a+b)^4)/(sqrt(11)+1)) <<endl;
+
+    //cout << "wynik pkt c to: " << (((cos(a+1))/(sqrt(5)+3))^3) <<endl;
+
+    cout << "Koniec za 3.4" <<endl;
+
+
+
+}
+
+
+
+
+
+
