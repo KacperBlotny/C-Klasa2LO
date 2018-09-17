ce4f84f69b7bc005e465dd19b7af887a0a96ce81
Kacper <43350503+KacperBlotny@users.noreply.github.com>
2018-09-17 20:57:54 +0200

    Add files via upload

---
+++ zaddom sophiechapelle.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Zadanie 3.3." << endl;
+    cout << "wynik a)" << 3*7/2-1 << endl;
+    cout << "wynik b)" << 3*7.0/2-1 << endl;
+    cout << "wynik c)" << (3*7)/2-1 << endl;
+    cout << "wynik d)" << 3*(7/2.0-1) << endl;
+    cout << "wynik e)" << 3*(7/2)-1 << endl;
+    cout << "wynik f)" << 3*7.0/(2-1) << endl;
+    cout << "wynik g)" << 124%12-2*4 << endl;
+    cout << "wynik h)" << 4*105%10/2*108 << endl;
+    cout << "wynik i)" << (-8-13*2)/2.5 << endl;
+    cout << "wynik j)" << (pow(2.5,3)+17%5)-sqrt (4) << endl;
+    cout << "wynik k)" << 12%5+2%5 << endl;
+    cout << "wynik l)" << 5+4*3.0/5 << endl;
+    cout << "wynik m)" << 2%5*3-4 << endl;
+    cout << "wynik n)" << 7/3%2+6 << endl;
+    cout << "wynik o)" << 10-4+2/2*3 << endl;
+    cout << "wynik p)" << 11/3%5+1 << endl;
+    cout << "wynik q)" << 4-5/4.0*2 <<endl;
+cout << endl;
+using namespace std;
+int a, b, c;
+ cout << "Zadanie 3.4." <<endl;
+    cout << endl;
+    cout << "wynik a)" << sqrt(7/((a^3)+cos(b))) << endl;
+    cout << "wynik b)" << sin (((a+b)^4)/(sqrt(11)+1)) << endl;
+    //cout << "wynik c)" << (((cos(a+1))/(sqrt(5)+3))^3) << endl;
+}
+
