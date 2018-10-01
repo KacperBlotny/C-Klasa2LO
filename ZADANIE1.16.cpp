5fdc306d5aa23db73b446c5764294fd2cc7359cf
Kacper <43350503+KacperBlotny@users.noreply.github.com>
2018-10-01 23:51:20 +0200

    Update ZADANIE1.16.cpp

--- ZADANIE1.16.cpp
+++ ZADANIE1.16.cpp
@@ -1,48 +1,54 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
 int main()
 
 {
-   int a, x;
 
-        cout << "Wybierz funkcje od 1-4"        <<endl;
+  int  x, a;
+      cout << "Wybierz opcje od 1-5"                                      <<endl;
+        cin  >> a;
 
-            cin   >> a;
+    while(x != 0)
 
-        while(x != 0)
 
-{
+    {
 
-            switch(a)
+        switch(a)
 
- {
+
+        {
+
+
     case 1: cin >> x;
-        if(x<7) cout << (pow(x,3)+1)                           <<endl;
-            else cout << "Nie ma takiej funkcji"               <<endl; break;
+        if(x<1) cout << 2*x                             <<endl;
+                else cout << "Nie ma takiej funkcji"        <<endl; break;
 
     case 2: cin >> x;
-        if(x==7) cout << cos(x-1)                              <<endl;
-            else cout << "Nie ma takiej funkcji"               <<endl; break;
+        if(x==1) cout << -10                            <<endl;
+                else cout << "Nie ma takiej funkcji"         <<endl; break;
 
     case 3: cin >> x;
-        if(x==9) cout << sqrt(3*x)                             <<endl;
-            else cout << "Nie ma takiej funkcji"               <<endl; break;
+        if(x==3) cout << pow(x-1,4)                     <<endl;
+                else cout << "Nie ma takiej funkcji"         <<endl; break;
 
     case 4: cin >> x;
-        if(x>7 && x!=9) cout << -8*x                           <<endl;
-            else cout << "Nie ma takiej funkcji"               <<endl; break;
+        if(x==6) cout << sqrt(x-4)                      <<endl;
+                else cout << "Nie ma takiej funkcji"         <<endl; break;
+
+    case 5: cin >> x;
+        if(x!=1&&x!=3&&x!=6&&x>1) cout << 0             <<endl;
+                else cout << "Nie ma takiej funkcji"         <<endl; break;
 
 
-               default: cout << "ERROR"                                      <<endl;
+    default: cout << "Blad"
 
 
+        }
 
- }
 
- }
+    }
 
 
     return 0;
