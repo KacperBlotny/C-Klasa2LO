9d903df87f53bd006ee25d3f3d59036d7d207dc1
Kacper <43350503+KacperBlotny@users.noreply.github.com>
2018-10-02 23:58:01 +0200

    Update zadanie 1.17.cpp

--- zadanie 1.17.cpp
+++ zadanie 1.17.cpp
@@ -3,47 +3,31 @@
 
 using namespace std;
 
+
 int main()
-
 {
-   int a, x;
+    int x;
 
-        cout << "Wybierz funkcje od 1-4"        <<endl;
-
-            cin   >> a;
+    cout << "\nPodaj wartosc funkcji\n" <<endl;
 
         while(x != 0)
-
-{
-
-            switch(a)
-
- {
-    case 1: cin >> x;
-        if(x<7) cout << (pow(x,3)+1)                           <<endl;
-            else cout << "Nie ma takiej funkcji"               <<endl; break;
-
-    case 2: cin >> x;
-        if(x==7) cout << cos(x-1)                              <<endl;
-            else cout << "Nie ma takiej funkcji"               <<endl; break;
-
-    case 3: cin >> x;
-        if(x==9) cout << sqrt(3*x)                             <<endl;
-            else cout << "Nie ma takiej funkcji"               <<endl; break;
-
-    case 4: cin >> x;
-        if(x>7 && x!=9) cout << -8*x                           <<endl;
-            else cout << "Nie ma takiej funkcji"               <<endl; break;
+       {
 
 
-               default: cout << "ERROR"                                      <<endl;
+    if (x<7) cout << (x*x*x)+1 <<endl;
+
+     else switch(x)
+
+    {
+        case 7: cout << "\n" <<cos(x-1)  <<endl; break;
+
+        case 9: cout << "\n" << sqrt(3*x) <<endl; break;
+
+        default: cout << "\n" << -1*(8*x) <<endl; break;
 
 
-
- }
-
- }
-
-
+    }
+            cin >> x;
+        }
     return 0;
 }
